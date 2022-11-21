90c0a6967f72cc653bbda6bc589c5e3e606e338f
Matt Rothenberg <mattrothenberg@gmail.com>
2022-11-21 18:54:52 +0100

    stub set state call

--- source/ui.tsx
+++ source/ui.tsx
@@ -1,40 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, Box } from "ink";
 import create from "zustand";
 import SelectInput from "ink-select-input";
 
 type AppStore = {
-	fruit: string;
-	setFruit: (fruit: string) => void;
+  fruit: string;
+  setFruit: (fruit: string) => void;
 };
 
 const useAppStore = create<AppStore>((set) => ({
-	fruit: "mango",
-	setFruit: (newFruit) => {
-		set(() => ({ fruit: newFruit }));
-	},
+  fruit: "mango",
+  setFruit: (newFruit) => {
+    set(() => ({ fruit: newFruit }));
+  },
 }));
 
 const App = () => {
-	const { fruit, setFruit } = useAppStore();
+  const { fruit, setFruit } = useAppStore();
+  const [_, setState] = useState(0);
 
-	return (
-		<Box flexDirection="column">
-			<Text>Selected fruit: {fruit}</Text>
-			<SelectInput
-				items={["apple", "mango", "orange", "pineapple"].map((mode) => {
-					return {
-						label: mode,
-						value: mode,
-					};
-				})}
-				onSelect={(item) => {
-					setFruit(item.value);
-					// setState(state + 1);
-				}}
-			/>
-		</Box>
-	);
+  return (
+    <Box flexDirection="column">
+      <Text>Selected fruit: {fruit}</Text>
+      <SelectInput
+        items={["apple", "mango", "orange", "pineapple"].map((mode) => {
+          return {
+            label: mode,
+            value: mode,
+          };
+        })}
+        onSelect={(item) => {
+          setFruit(item.value);
+          setState((curr) => curr + 1);
+        }}
+      />
+    </Box>
+  );
 };
 
 module.exports = App;
